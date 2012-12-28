ab84b16dfa3d631917cdcb2a0166349277d24917
Alex <alex.sun@oocl.com>
2012-12-28 17:54:14 +0800

    show bottom components

--- src/view/HorizontalPanel.java
+++ src/view/HorizontalPanel.java
@@ -1,18 +1,71 @@
 package view;
 
+import java.awt.Color;
+import java.awt.Cursor;
 import java.awt.Dimension;
-import java.awt.GridLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.io.FileNotFoundException;
 
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+
+import util.ImageUtil;
 
 public class HorizontalPanel extends JPanel {
 
 	private static final long serialVersionUID = 6677235068546682813L;
 
-	public HorizontalPanel(Dimension preferredSize) {
+	private JLabel lblRoll;
+	
+	private JLabel lblDraw;
+	
+	private JLabel lblRedraw;
+	
+	public HorizontalPanel(Dimension preferredSize) throws FileNotFoundException {
 		super();
 		this.setPreferredSize(preferredSize);
 		this.setOpaque(false);
-		this.setLayout(new GridLayout());
+		this.setLayout(new GridBagLayout());
+		
+		initComponents();
+		
+		GridBagConstraints c = new GridBagConstraints();
+		c.ipadx = 0;
+		c.gridx = 0;
+		c.insets = new Insets(0, -50, 0, 70);
+		c.anchor = GridBagConstraints.CENTER;
+		c.fill = GridBagConstraints.BOTH;
+		this.add(lblRoll, c);
+		
+		c.insets = new Insets(0, 0, 0, 10);
+		c.ipadx = 0;
+		c.gridx = 1;
+		c.anchor = GridBagConstraints.CENTER;
+		c.gridwidth = GridBagConstraints.RELATIVE;
+		this.add(lblDraw, c);
+		
+		c.gridx = 2;
+		c.gridwidth = GridBagConstraints.REMAINDER;
+		this.add(lblRedraw, c);
 	}
+	
+	private void initComponents() throws FileNotFoundException {
+		lblRoll = new JLabel();
+		lblRoll.setPreferredSize(new Dimension(200, 50));
+		lblRoll.setOpaque(true); //for test
+		lblRoll.setBackground(Color.ORANGE); //for test
+		
+		ImageIcon icoDraw = ImageUtil.loadImgIcon("draw.png");
+		lblDraw = new JLabel(icoDraw);
+		lblDraw.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		
+		ImageIcon icoRedraw = ImageUtil.loadImgIcon("redraw.png");
+		lblRedraw = new JLabel(icoRedraw);
+		lblRedraw.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		
+	}
+	
 }
